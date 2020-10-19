28363fceeb35efd6dd00e3f5bafd1fff9d198cf7
TransformsGit <64213593+TransformsGit@users.noreply.github.com>
2020-10-19 23:58:51 +0300

    Add files via upload

---
+++ 1restore_snapshot_152_win.bat
@@ -0,0 +1,22 @@
+echo off
+set machine=172.27.105.150
+echo %machine%
+set password=Passw0rd!
+echo %password%
+set user_name=root
+echo %user_name%
+
+   cd C:\plink\
+  
+   echo connecting to %machine% ...  via ssh...
+   echo %cd%
+   echo y | C:\plink\plink.exe -ssh %machine% -l %root% -pw %password% 
+   C:\plink\plink.exe -batch -hostkey 6d:57:d4:46:08:34:96:db:c7:fe:87:6d:10:b3:e4:ab -sshlog ssh.log -m restore_img152.txt %user_name%@%machine% -pw %password%  >>log_ssh.txt
+
+
+echo STOP
+echo STOP
+echo STOP
+
+
+echo EXIT
